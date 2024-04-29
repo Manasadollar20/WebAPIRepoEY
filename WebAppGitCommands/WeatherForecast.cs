22f381e30756141cdbbf7529836f01a9303807f0
Manasadollar20 <168421269+Manasadollar20@users.noreply.github.com>
2024-04-29 18:24:26 +0530

    conc 5

--- WebAppGitCommands/WeatherForecast.cs
+++ WebAppGitCommands/WeatherForecast.cs
@@ -13,5 +13,7 @@ namespace WebAppGitCommands
         public string Conclusion { get; set; }
 
         public string Conclusion3 {get; set;}
+
+        public string Conclusion5 {get; set;}
     }
 }
