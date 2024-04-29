8e8d0c7ab4e8d4c47c576d0b0e8f54cf7e16b926
Manasadollar20 <manasanettimi2005@gmail.com>
2024-04-29 18:03:04 +0530

    controller changed

--- WebAppGitCommands/WeatherForecast.cs
+++ WebAppGitCommands/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace WebAppGitCommands
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string Conclusion { get; set; }
     }
 }
